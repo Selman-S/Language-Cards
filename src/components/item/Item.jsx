0832aa491505a509de051fc255f476fea34bf0ec
selman <gunduzgece546@gmail.com>
2022-06-30 13:39:00 +0300

    set timeout added

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -4,9 +4,13 @@ import './Item.css'
 const Item = ({card}) => {
   const {name,img,options}= card
   const [toggle,setToggle]= useState(true)
+  const handleCardVisible = (e)=> {
+    setToggle(!toggle);
+    setTimeout(()=> setToggle(true),1500)
+    console.log(e.target)
+  }
   return (
-    <div className="card" onClick={() =>setToggle(!toggle)
-    } >
+    <div className="card" onClick={handleCardVisible} >
       {toggle ?<div>
       <img src={img} alt="log" />
       <h5>{name}</h5>
